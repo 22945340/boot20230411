0f102df36a6bd5791da1d6cb7cbce8602a41845c
Mino1157 <121426824+Mino1157@users.noreply.github.com>
2023-04-27 14:41:53 +0900

    a

---
+++ src/main/java/com/example/demo/component/ComponentC.java
@@ -0,0 +1,13 @@
+package com.example.demo.component;
+
+public class ComponentC {
+	private ComponentB comp;
+	
+	public void SetComp(ComponentB comp) {
+		this.comp = comp;
+	}
+	
+	public ComponentB GetComp() {
+		return comp;
+	}
+}
---
+++ src/main/java/com/example/demo/mapper/Mapper06.java
@@ -0,0 +1,24 @@
+package com.example.demo.mapper;
+
+import java.util.*;
+
+import org.apache.ibatis.annotations.*;
+
+import com.example.demo.domain.*;
+
+@Mapper
+public interface Mapper06 {
+
+	@Select("""
+			SELECT 
+				CustomerId id, 
+				CustomerName name,
+				Contactname,
+				Address  
+			FROM Customers
+			ORDER BY CustomerId DESC
+			Limit #{startIndex}, 20
+			""")
+	List<Customer> listCustomer(Integer startIndex);
+
+}
